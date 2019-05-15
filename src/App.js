0dce639ed072b65b2dbde952fe326a724af7183c
sumit gupta <sumit4palz@gmail.com>
2019-05-15 13:55:20 +0530

    Organizing source folder by features

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import WeeklyMealPlan from './components/weeklymealplan'
-
-ReactDOM.render(<WeeklyMealPlan />, document.getElementById("root"));
-
-export default WeeklyMealPlan;
---
+++ src/landing/landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './landing.css';
+
+
+function LandingPage(props){
+    return(
+        <div>
+            <h1>I am landing page</h1>
+        </div>
+    );
+}
+
+export default LandingPage;
