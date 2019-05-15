fdf1253ccea7a3e0fcb0ea8428c5e8588bad63f1
sumit gupta <sumit4palz@gmail.com>
2019-05-15 17:59:55 +0530

    Adding links to header

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-function Header(props){
-    return(
-       <label>Header</label>
+function Header(props) {
+    return (
+        <div>
+            <img src="logo.png" alt="mealplanner" />
+            <nav>
+                <ul>
+                    <li><a href="bikes">Features</a></li>
+                    <li><a href="bikes/bmx">About</a></li>
+                    <li><a href="bikes/bmx">Contact</a></li>
+                    <li><a href="bikes/bmx">Signup</a></li>
+                    <li>Or</li>
+                    <li><a href="bikes/bmx">Login</a></li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
